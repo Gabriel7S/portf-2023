334e2528378496af81543aec67db21989e8d3a1b
Gabriel da Silva <98197076+Gabriel7S@users.noreply.github.com>
2023-08-29 11:24:36 -0300

    terceiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css'
-<<<<<<< HEAD
 import AppRoutes from './routes'
+import Home from './pages/Home'
 
 function App() {
   return (
    <AppRoutes/>
-=======
-import Home from './pages/Home'
+  )
+
 
 function App() {
 
   return (
    <Home/>
->>>>>>> ecaa45fb1f08e40af10df382cf5e01dba916de95
   )
+}
 }
 
 export default App
